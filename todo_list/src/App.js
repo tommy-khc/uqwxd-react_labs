24a0e883f7f7c44e781d7a80ee03262ae11d5f8d
tommy-khc <tomy123abc@gmail.com>
2024-02-26 12:40:00 -0800

    completed

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [todoEditing, setTodoEditing] = useState(null);
+
+  // Add the useEffect code here
+  useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      const json = JSON.stringify(todos);
+      localStorage.setItem("todos", json);
+    }
+  }, [todos]);
 
   // Add the handlesubmit code here
   function handleSubmit(e) {
@@ -22,10 +40,33 @@ const App = () => {
   }
 
   // Add the deleteToDo code here
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
 
   // Add the toggleComplete code here
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
 
   // Add the submitEdits code here
+  function submitEdits(newtodo) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === newtodo.id) {
+        todo.text = document.getElementById(newtodo.id).value;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+  }
 
   return (
     <div id="todo-list">
@@ -35,9 +76,32 @@ const App = () => {
         <button type="submit">Add Todo</button>
       </form>
       {todos.map((todo) => (
-        <div className="todo" key={todo.id}>
-          <div className="todo-text">{todo.text}</div>
-          <button type="delete">Delete</button>
+        <div key={todo.id} className="todo">
+          <div className="todo-text">
+            {/* Add checkbox for toggle complete */}
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+            {/* if it is edit mode, display input box, else display text */}
+            {todo.id === todoEditing ? (
+              <input type="text" id={todo.id} defaultValue={todo.text} />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todo-actions">
+            {/* if it is edit mode, allow submit edit, else allow edit */}
+            {todo.id === todoEditing ? (
+              <button onClick={() => submitEdits(todo)}>Submit Edits</button>
+            ) : (
+              <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+            )}
+
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
